f13730237214edbd4357a5a67f5fd513ecc5e90a
nickzapryagaev <impulse.nn@gmail.com>
2022-10-26 23:19:41 +0600

    7. Расстояние между точками в системе координат [#188 #350467]

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -36,4 +36,24 @@ public class PointTest {
         double eps = 0.01;
         assertThat(input).isCloseTo(expected, offset(eps));
     }
+
+    @Test
+    public void when1031to232then8() {
+        Point a = new Point(10, 3, 1);
+        Point b = new Point(2, 3,3);
+        double input = a.distance3d(b);
+        double expected = 8.24;
+        double eps = 0.01;
+        assertThat(input).isCloseTo(expected, offset(eps));
+    }
+
+    @Test
+    public void when626to826then2() {
+        Point a = new Point(6, 2, 6);
+        Point b = new Point(8, 2,6);
+        double input = a.distance3d(b);
+        double expected = 2;
+        double eps = 0.01;
+        assertThat(input).isCloseTo(expected, offset(eps));
+    }
 }
