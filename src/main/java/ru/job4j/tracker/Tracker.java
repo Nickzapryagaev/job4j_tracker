f033e6fd5e1fbeb8491b65fb2aff831613ca289e
nickzapryagaev <impulse.nn@gmail.com>
2022-10-30 14:00:29 +0600

    5. Tracker - хранилище [#396 #356024]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -30,15 +30,14 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        Item[] itemsCut = findAll();
-        Item[] rst = new Item[itemsCut.length];
-        int size = 0;
-        for (int i = 0; i < itemsCut.length; i++) {
-            if (key.equals(itemsCut[i].getName())) {
-                rst[size] = itemsCut[i];
-                size++;
+        Item[] rst = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (key.equals(items[i].getName())) {
+                rst[count] = items[i];
+                count++;
             }
         }
-        return Arrays.copyOf(rst, size);
+        return Arrays.copyOf(rst, count);
     }
 }
