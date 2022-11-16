a6ffc83a5d6140108aa5f667c3eba14c44c0da0c
Nickzapryagaev <impulse.nn@gmail.com>
2022-11-16 20:43:36 +0600

    3. Модели данных и HashSet. [#218530]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NotifyAccountTest {
+
     @Test
     public void whenTwoAccountsAddSent() {
         NotifyAccount notifyAccount = new NotifyAccount();
