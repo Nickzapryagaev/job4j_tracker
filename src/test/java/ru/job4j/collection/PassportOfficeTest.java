46c4986fb448c612c67a21191fb410b975a31607
Nickzapryagaev <impulse.nn@gmail.com>
2022-11-17 21:11:41 +0600

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethodTrue() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    public void whenTestAddMethodFalse() {
+        Citizen petr = new Citizen("2f44a", "Petr Arsentev");
+        Citizen nick = new Citizen("2f44a", "Nick Zap");
+        PassportOffice office = new PassportOffice();
+        office.add(petr);
+        assertThat(office.add(nick)).isFalse();
+    }
+}
